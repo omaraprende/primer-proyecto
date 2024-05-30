20d7941cfb9255e05e90fd93af65877464177f06
Omar <omar.igual@inetum.com>
2024-05-30 13:04:54 +0200

    first commit

---
+++ src/app/models/pelicula.model.ts
@@ -0,0 +1,13 @@
+export interface PeliculaModel {
+    id: number;
+    titulo: string;
+    director: string;
+    fechaEstreno: Date;
+    duracion: number;
+    genero: string;
+    sinopsis: string;
+    imagen: string;
+    precio: number;
+    rating: number;
+}
+
